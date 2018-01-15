b302c97bf0b9bcc3d5fd2249c04b7db04f5b2914
Doug Smith <smith-doug@users.noreply.github.com>
2018-01-15 12:34:45 -0600

    Add simple interpolator
    It will interpolate between positions in a fixed amount of time.  No speed/accel.  Small movements with lots of points will probably time out.

--- rmi_driver/scripts/server_test.py
+++ rmi_driver/scripts/server_test.py
@@ -2,6 +2,8 @@
 import socket
 import sys
 import signal
+import rospy
+import copy
 
 import threading
 import time
@@ -16,11 +18,41 @@ class RobotData():
             self.joint_pos += '0.' + str(i) + ' '
         self.joint_pos = self.joint_pos.strip()
 
+        self.target_pos = copy.deepcopy(self.joint_pos)
+        self.is_moving = False
+        self.inter_step = 0
+        self.start_pos = copy.deepcopy(self.joint_pos)
+
 
 class RmiServer(SocketServer.StreamRequestHandler):
     allow_reuse_address = True
 
+    def __init__(self, request, client_address, server):
+
+        self.step_target = 20.0  # number of steps to use in interpolation
+
+        SocketServer.StreamRequestHandler.__init__(self, request, client_address, server)
+
+    def interpolate(self, target_pt, start_pt, step):
+        if step >= self.step_target:
+            self.server.robot_data.is_moving = False
+            return target_pt
+
+        start_num = [float(n) for n in start_pt.split()]
+        target_num = [float(n) for n in target_pt.split()]
+
+        inter_pt = []
+        for target_joint, start_joint in zip(target_num, start_num):
+            inter_pt.append(start_joint + (step / self.step_target) * (target_joint - start_joint))
+
+        ret_val = ''
+        for pt in inter_pt:
+            ret_val += str(pt) + ' '
+
+        return ret_val.strip()
+
     def handle(self):
+        print "New connection from " + str(self.client_address)
 
         while True:
             try:
@@ -30,17 +62,47 @@ class RmiServer(SocketServer.StreamRequestHandler):
                     print data
 
                 send_data = 'OK'
+                if data.startswith('abort'):
+                    self.server.robot_data.is_moving = False
+                    self.server.robot_data.target_pos = self.server.robot_data.joint_pos
+                    self.server.robot_data.inter_step = 10000.0
+
+                    send_data = 'aborted'
+
                 if data.startswith('get version'):
                     send_data = '0.0.7'
                 elif data.startswith('get joint position'):
+
+                    self.server.robot_data.joint_pos = self.interpolate(self.server.robot_data.target_pos,
+                                                                        self.server.robot_data.start_pos, self.server.robot_data.inter_step)
+
+                    if self.server.robot_data.inter_step <= self.step_target:
+                        self.server.robot_data.inter_step += 1
+
                     send_data = self.server.robot_data.joint_pos
+
                 elif data.startswith('get tool frame ros'):
                     send_data = '0.0 0.1 0.2 0.3 0.4 0.5'
 
                 elif data.startswith('ptp joints'):
                     words = data.split(';')
                     values = words[0].split(':')[1].strip()
-                    self.server.robot_data.joint_pos = values
+
+                    self.server.robot_data.start_pos = copy.deepcopy(self.server.robot_data.joint_pos)
+                    self.server.robot_data.inter_step = 0
+                    self.server.robot_data.target_pos = values
+                    self.server.robot_data.is_moving = True
+
+                    while self.server.robot_data.inter_step < (self.step_target / 1.2):
+                        time.sleep(0.001)
+
+                    # continue
+
+                    #self.server.robot_data.joint_pos = values
+                elif data.startswith('wait is_finished'):
+                    while self.server.robot_data.is_moving:
+                        time.sleep(0.001)
+                    send_data = 'OK'
                 else:
                     send_data = 'ERROR not implemented'
 
@@ -48,8 +110,8 @@ class RmiServer(SocketServer.StreamRequestHandler):
                     send_data += '\n'
 
                 self.wfile.write(send_data)
-            except:
-                print 'except in RmiServer handle'
+            except Exception as ex:
+                print 'except in RmiServer handle ' + str(ex)
 
                 break
 
